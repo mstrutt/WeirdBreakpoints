53feb452a3d4db561a90b7fc79deadf3f62c7bdc
Michael Strutt <michael@mstrutt.co.uk>
2013-09-10 21:13:42 +0100

    updating stats to pull in the past month dynamically

--- stats.php
+++ stats.php
@@ -1,3 +1,6 @@
 <?php
-	echo file_get_contents('http://gs.statcounter.com/chart.php?bar=1&statType_hidden=mobile_resolution&region_hidden=ww&granularity=monthly&statType=Mobile%20Screen%20Resolution&region=Worldwide&fromMonthYear=2013-08&toMonthYear=2013-08');
+	$datestring = 'first day of last month';
+	$lastMonth = date_create($datestring);
+
+	echo file_get_contents('http://gs.statcounter.com/chart.php?bar=1&statType_hidden=mobile_resolution&region_hidden=ww&granularity=monthly&statType=Mobile%20Screen%20Resolution&region=Worldwide&fromMonthYear='.$lastMonth->format('Y').'-'.$lastMonth->format('m').'&toMonthYear='.date('Y').'-'.date('d')));
 ?>
