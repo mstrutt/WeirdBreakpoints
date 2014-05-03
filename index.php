ddc89f3e98f008e8b835aa2bf35411b71760bd15
Michael Strutt <michael@mstrutt.co.uk>
2014-05-03 13:18:28 +0100

    adding GA script

--- index.php
+++ index.php
@@ -86,5 +86,14 @@
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js" defer></script>
 		<script defer>window.jQuery || document.write('<script src="/assets/scripts/lib/jquery.min.js"><\/script>')</script>
 		<script src="/assets/scripts/site.js" defer></script>
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-32384890-2', 'weird-breakpoints.com');
+			ga('send', 'pageview');
+		</script>
 	</body>
 </html>
